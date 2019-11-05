cc14bfb12e6479e79349d812d8c3534e26cf5853
Artem <tor003.llllll@maik.ru>
2019-11-05 20:37:54 +0300

    Добавлен переход на VC полного рецепта с передачей данных

---
+++ RecipeSelector/RecipeSelector/FullRecipe.swift
@@ -0,0 +1,30 @@
+//
+//  FullRecipe.swift
+//  RecipeSelector
+//
+//  Created by Артем on 28/10/2019.
+//  Copyright © 2019 ArtemTkachuk. All rights reserved.
+//
+
+import UIKit
+
+class FullRecipe: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
