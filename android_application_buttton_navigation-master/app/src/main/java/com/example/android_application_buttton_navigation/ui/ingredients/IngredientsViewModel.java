71dfc5bbde932db3f6230145462fbd0ec3eb59f5
Denis <durmadon@list.ru>
2020-01-03 13:32:38 +0300

    добавлена анимация свайпа кнопки в MyIngredientFragment

--- android_application_buttton_navigation-master/app/src/main/java/com/example/android_application_buttton_navigation/ui/ingredients/IngredientsViewModel.java
+++
@@ -1,9 +0,0 @@
-package com.example.android_application_buttton_navigation.ui.ingredients;
-
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
-public class IngredientsViewModel extends ViewModel {
-
-}
