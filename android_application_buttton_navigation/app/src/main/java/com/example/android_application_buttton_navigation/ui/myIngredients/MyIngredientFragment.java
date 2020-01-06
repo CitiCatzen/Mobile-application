8ff4cd700486a63419756e8c9d337eea4f7ea8ed
Денис <denismyasnikov@bk.ru>
2020-01-07 00:05:35 +0300

    Добавлен поиск ингредиента по названию в 'Мои ингредиенты'

--- android_application_buttton_navigation/app/src/main/java/com/example/android_application_buttton_navigation/ui/myIngredients/MyIngredientFragment.java
+++ android_application_buttton_navigation/app/src/main/java/com/example/android_application_buttton_navigation/ui/myIngredients/MyIngredientFragment.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.android_application_buttton_navigation.Ingredient;
 import com.example.android_application_buttton_navigation.MainActivity;
@@ -21,8 +25,9 @@ import java.util.ArrayList;
 
 import static java.lang.StrictMath.abs;
 
-public class MyIngredientFragment extends Fragment implements View.OnTouchListener {
+public class MyIngredientFragment extends Fragment implements View.OnTouchListener, TextView.OnEditorActionListener {
     private LinearLayout linearLayout;
+    private EditText ingredientName;
     private ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
     float staticX = 0;
     LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
@@ -32,18 +37,10 @@ public class MyIngredientFragment extends Fragment implements View.OnTouchListen
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.my_ingredient_fragment, container, false);
         linearLayout = root.findViewById(R.id.linMyIngredients);
+        ingredientName = root.findViewById(R.id.ingredientName);
+        ingredientName.setOnEditorActionListener(this);
         ingredients = MainActivity.getIngredients();
-        for(int i = 0; i<ingredients.size(); i++ ){
-            if (ingredients.get(i).getIsExist()){
-                Button button = new Button(getContext());
-                button.setLayoutParams(layoutParams);
-                button.setOnTouchListener(this);
-                button.setText(ingredients.get(i).getName());
-                button.setId(ingredients.get(i).getId());
-                linearLayout.addView(button);
-
-            }
-        }
+        pageRefresh();
         return root;
     }
 
@@ -68,4 +65,42 @@ public class MyIngredientFragment extends Fragment implements View.OnTouchListen
         }
         return false;
     }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_SEARCH){
+            pageRefresh(ingredientName.getText().toString());
+            return true;
+        }
+        return false;
+    }
+
+    private void pageRefresh(){
+        linearLayout.removeAllViews();
+        for(int i = 0; i<ingredients.size(); i++ ){
+            if (ingredients.get(i).getIsExist()){
+                Button button = new Button(getContext());
+                button.setLayoutParams(layoutParams);
+                button.setOnTouchListener(this);
+                button.setText(ingredients.get(i).getName());
+                button.setId(ingredients.get(i).getId());
+                linearLayout.addView(button);
+            }
+        }
+    }
+
+    private void pageRefresh(String name){
+        linearLayout.removeAllViews();
+        for(int i = 0; i<ingredients.size(); i++ ){
+            if (ingredients.get(i).getIsExist() && ingredients.get(i).getName().contains(name)){
+                Button button = new Button(getContext());
+                button.setLayoutParams(layoutParams);
+                button.setOnTouchListener(this);
+                button.setText(ingredients.get(i).getName());
+                button.setId(ingredients.get(i).getId());
+                linearLayout.addView(button);
+            }
+        }
+    }
+
 }
